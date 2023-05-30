1dd51ca9a41bc9cc7281df6e7742079e9c35e84b
uma <umabaligranado@gmail.com>
2023-05-30 19:15:22 -0300

    avanzado, falta notices, y fix root

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_30_145527) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_30_205134) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_30_145527) do
     t.string "phone"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_contacts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
